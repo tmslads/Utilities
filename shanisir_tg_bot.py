0f4f0669062c36f1c468c4c4dfa5c4b3f41a3014
Harshil <37377066+harshil21@users.noreply.github.com>
2019-12-04 00:41:37 +0400

    added help command, improved sorting of results
    also removed unnecessary commands

--- shanisir_tg_bot.py
+++ shanisir_tg_bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from difflib import get_close_matches
 from uuid import uuid4
 
 from telegram import InlineQueryResultCachedAudio
@@ -13,34 +14,42 @@ clipLocation = f"{loc}\\Assets\\clips"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-updater = Updater(token='<INSERT API KEY HERE>', use_context=True)
+updater = Updater(token='<API KEY>', use_context=True)
 dispatcher = updater.dispatcher
 
 results = []
 
+with open("file_ids.txt", "r") as ids, open("names.txt", "r") as name:
+    file_ids = ids.read().strip().split(',')
+    names = name.read().strip().split(',')
+
+assert (len(names) == len(file_ids))
+for file_id, name in zip(file_ids, names):
+    results.append(InlineQueryResultCachedAudio(
+        id=uuid4(),
+        audio_file_id=file_id,
+        caption=name))
+
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="I'm an inline bot, @ me in the chatbox and type to get a audio clip")
+
+
+def helper(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="This bot sends you actual shani sir clips straight from Shanisirmodule! He is savage"
+                                  " in groups too! More commands will be added in the future."
+                                  " P.S: Download Shanisirmodule from:"
+                                  " https://github.com/tmslads/Shanisirmodule/releases")
 
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-
-def clips(update, context):
-    clipss = os.listdir(clipLocation)
-    for clip in clipss:
-        c = context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(f'{clipLocation}\\{clip}', 'rb'),
-                                   title=f"{clip[:-4]}")
-        results.append(InlineQueryResultCachedAudio(
-            id=uuid4(),
-            audio_file_id=c['audio']['file_id'],
-            caption=c['audio']['title']))
+def secret(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="stop finding secret commands :P")
 
 
 def inline_clips(update, context):
@@ -48,8 +57,17 @@ def inline_clips(update, context):
     if not query:
         return
     else:
-        matches = (result for result in results if query in result['caption'])
-        context.bot.answer_inline_query(update.inline_query.id, matches, next_offset="10")
+        matches = get_close_matches(query, names, n=15, cutoff=0.4)
+        index = 0
+        while index <= len(matches) - 1:
+            for pos, result in enumerate(results):
+                if index == len(matches):
+                    break
+                if matches[index] == result['caption']:
+                    results[index], results[pos] = results[pos], results[index]
+                    index += 1
+
+        context.bot.answer_inline_query(update.inline_query.id, results[:16])
 
 
 def unknown(update, context):
@@ -59,10 +77,10 @@ def unknown(update, context):
 inline_clips_handler = InlineQueryHandler(inline_clips)
 dispatcher.add_handler(inline_clips_handler)
 
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
+help_handler = CommandHandler('help', helper)
+dispatcher.add_handler(help_handler)
 
-clip_handler = CommandHandler('all', clips)
+clip_handler = CommandHandler('secret', secret)
 dispatcher.add_handler(clip_handler)
 
 start_handler = CommandHandler('start', start)
