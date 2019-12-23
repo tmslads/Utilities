ff9e79aca4a9823c00d25abd2e446a0c7e55f857
Harshil <37377066+harshil21@users.noreply.github.com>
2019-12-24 02:12:43 +0400

    imported shaniTTS code for converting given messages
    it's like a mini shanisirmodule

--- shanisir_tg_bot.py
+++ shanisir_tg_bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 from difflib import get_close_matches
 from uuid import uuid4
 
@@ -8,13 +9,14 @@ from telegram.ext import CommandHandler
 from telegram.ext import InlineQueryHandler
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
+from textblob import TextBlob
 
 loc = os.path.dirname(os.path.abspath('__Shanisirmodule__'))
 clipLocation = f"{loc}\\Assets\\clips"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-updater = Updater(token='<API KEY>', use_context=True)
+updater = Updater(token='997899425:AAEvgyU_vFECOBRGMB2FYxGr_yONMeoJk58', use_context=True)
 dispatcher = updater.dispatcher
 
 results = []
@@ -45,7 +47,65 @@ def helper(update, context):
 
 
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    cleaned = []
+    JJ_RB = ["like you say", "like you speak", "not hard", "okay, fine?"]  # For Adjectives or Adverbs
+
+    msg = update.message.text
+    if update.message.text == '':
+        msg = "Arey you didn't give me anything to say you donut buffalo!"
+
+    blob = TextBlob(msg)
+    cleaned = blob.words  # Returns list with no punctuation marks
+    blob_tags_iter = iter(blob.tags)
+
+    flag = 0  # To check if a modal is present in the sentence
+    lydcount = 0  # Counts the number of times "like you do" has been added
+    JJ_RBcount = 0  # Counts the number of times a phrase from JJ_RB has been added
+    if len(cleaned) < 15:
+        lydlim = 2  # to limit the number of times we add
+        JJ_RBlim = 2  # lyd and JJ_RB
+    else:
+        lydlim = len(cleaned) // 9
+        JJ_RBlim = len(cleaned) // 9
+    for word, tag in blob_tags_iter:  # returns list of tuples which tells the POS
+        index = cleaned.index(word)
+
+        if tag == 'MD' and not flag:  # Modal
+            cleaned.insert(index + 1, "(if the laws of physics allow it)")
+            flag = 1
+
+        if tag in ['JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS'] and JJ_RBcount < JJ_RBlim:  # Adjective or Adverb
+            cleaned.insert(index + 1, random.choice(JJ_RB))
+            JJ_RBcount += 1
+
+        elif tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'] and lydcount < lydlim:  # Verb
+            cleaned.insert(index + 1, "like you do")
+            lydcount += 1
+
+    if random.choice([0, 1]):
+        cleaned.append(random.choice(["I am so sowry", "i don't want to talk like that", "*scratches nose*",
+                                      "it is embarrassing to me like basically", "it's not to trouble you like you say",
+                                      "go for the worksheet"]))
+    else:
+        cleaned.append(random.choice(["this will be fruitful", "you will benefit", "that is the expected behaviour",
+                                      "now you are on the track like", "class is in the flow like",
+                                      "aim to hit the tarjit",
+                                      "don't press the jockey"]))
+
+    cleaned.insert(0, 'good mourning')
+
+    if len(cleaned) < 5:  # Will run if input is too short
+        cleaned.append("*draws perfect circle*")
+
+    if 'when' in cleaned or 'When' in cleaned:  # If question is present in input then-
+        cleaned.append('decide a date')
+
+    shanitext = ' '.join(cleaned).capitalize()
+    temp = ''
+
+    print(update.message.text)
+    print(shanitext)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=shanitext)
 
 
 def secret(update, context):
@@ -55,7 +115,8 @@ def secret(update, context):
 def inline_clips(update, context):
     query = update.inline_query.query
     if not query:
-        return
+        random.shuffle(results)
+        context.bot.answer_inline_query(update.inline_query.id, results[:25])
     else:
         matches = get_close_matches(query, names, n=15, cutoff=0.4)
         index = 0
